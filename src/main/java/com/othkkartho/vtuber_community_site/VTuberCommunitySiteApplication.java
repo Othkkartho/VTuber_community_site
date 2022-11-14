a2df02f7b5c9e9b387e4063d43ecd0f683903506
Othkkartho <kimwonkyu15@gmail.com>
2022-11-14 11:46:57 +0900

    first setting code

---
+++ src/main/java/com/othkkartho/vtuber_community_site/VTuberCommunitySiteApplication.java
@@ -0,0 +1,13 @@
+package com.othkkartho.vtuber_community_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VTuberCommunitySiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VTuberCommunitySiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/othkkartho/vtuber_community_site/VTuberCommunitySiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.othkkartho.vtuber_community_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VTuberCommunitySiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
